7c8775a4db4941f0cde6526fb5de4af1ece4f90f
raydaysha <raydaysha@gmail.com>
2015-12-03 14:08:30 +0000

    needed to change directory of shell script

--- runDockerMaven.sh
+++ runDockerMaven.sh
@@ -1,4 +1,4 @@
 
-sudo docker run -d --name tomcat-maven -p 8080:8080 --link mongodb:mongodb dockertutorial/tomcat-maven:latest /local/git/docker-maven-tomcat/run.sh
+sudo docker run -d --name tomcat-maven -p 8080:8080 --link mongodb:mongodb dockertutorial/tomcat-maven:latest /local/git/DockerTutorialJavaMaven/run.sh
 
 # once running you can identify the IP addr by using: boot2docker ip. Then, use that IP to access port 8080
